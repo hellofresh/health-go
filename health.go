245f2503a8de45694a896acf2f320e957163e31f
Rafael Jesus <rafaelljesus86@gmail.com>
2018-03-13 11:53:56 +0100

    fix leaked chan

--- health.go
+++ health.go
@@ -8,64 +8,76 @@ import (
 	"time"
 )
 
-var mu sync.Mutex
-var checks []Config
+var (
+	mu     sync.Mutex
+	checks []Config
+)
 
 const (
 	statusOK                 = "OK"
 	statusPartiallyAvailable = "Partially Available"
 	statusUnavailable        = "Unavailable"
-
-	failureTimeout = "Timeout during health check"
+	failureTimeout           = "Timeout during health check"
 )
 
-// Config carries the parameters to run the check.
-type Config struct {
-	Name      string
-	Timeout   time.Duration
-	SkipOnErr bool
-	Check     func() error
-}
+type (
+	// CheckFunc is the func which executes the check.
+	CheckFunc func() error
 
-// Check represents the health check response.
-type Check struct {
-	Status    string            `json:"status"`
-	Timestamp time.Time         `json:"timestamp"`
-	Failures  map[string]string `json:"failures,omitempty"`
-	System    `json:"system"`
-}
+	// Config carries the parameters to run the check.
+	Config struct {
+		// Name is the name of the resource to be checked.
+		Name string
+		// Timeout is the timeout defined for every check.
+		Timeout time.Duration
+		// SkipOnErr if set to true, it will retrieve StatusOK providing the error message from the failed resource.
+		SkipOnErr bool
+		// Check is the func which executes the check.
+		Check CheckFunc
+	}
 
-// System runtime variables about the go process.
-type System struct {
-	Version          string `json:"version"`
-	GoroutinesCount  int    `json:"goroutines_count"`
-	TotalAllocBytes  int    `json:"total_alloc_bytes"`
-	HeapObjectsCount int    `json:"heap_objects_count"`
-	AllocBytes       int    `json:"alloc_bytes"`
-}
+	// Check represents the health check response.
+	Check struct {
+		// Status is the check status.
+		Status string `json:"status"`
+		// Timestamp is the time in which the check occurred.
+		Timestamp time.Time `json:"timestamp"`
+		// Failures holds the failed checks along with their messages.
+		Failures map[string]string `json:"failures,omitempty"`
+		// System holds information of the go process.
+		System `json:"system"`
+	}
 
-type checkResponse struct {
-	name      string
-	skipOnErr bool
-	err       error
-}
+	// System runtime variables about the go process.
+	System struct {
+		// Version is the go version.
+		Version string `json:"version"`
+		// GoroutinesCount is the number of the current goroutines.
+		GoroutinesCount int `json:"goroutines_count"`
+		// TotalAllocBytes is the total bytes allocated.
+		TotalAllocBytes int `json:"total_alloc_bytes"`
+		// HeapObjectsCount is the number of objects in the go heap.
+		HeapObjectsCount int `json:"heap_objects_count"`
+		// TotalAllocBytes is the bytes allocated and not yet freed.
+		AllocBytes int `json:"alloc_bytes"`
+	}
+
+	checkResponse struct {
+		name      string
+		skipOnErr bool
+		err       error
+	}
+)
 
 // Register registers a check config to be performed.
 func Register(c Config) {
 	mu.Lock()
 	defer mu.Unlock()
+
 	if c.Timeout == 0 {
 		c.Timeout = time.Second * 2
 	}
 	checks = append(checks, c)
-}
-
-// Reset unregisters all previously set check configs
-func Reset() {
-	mu.Lock()
-	defer mu.Unlock()
-
-	checks = []Config{}
 }
 
 // Handler returns an HTTP handler (http.HandlerFunc).
@@ -77,13 +89,19 @@ func Handler() http.Handler {
 func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	defer mu.Unlock()
+
 	status := statusOK
 	failures := make(map[string]string)
 	resChan := make(chan checkResponse, len(checks))
+	defer close(resChan)
 	for _, c := range checks {
-		go func(c Config, resChan chan checkResponse) {
-			resChan <- checkResponse{c.Name, c.SkipOnErr, c.Check()}
-		}(c, resChan)
+		go func(c Config) {
+			resChan <- checkResponse{
+				name:      c.Name,
+				skipOnErr: c.SkipOnErr,
+				err:       c.Check(),
+			}
+		}(c)
 
 	loop:
 		for {
@@ -119,6 +137,14 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
+// Reset unregisters all previously set check configs
+func Reset() {
+	mu.Lock()
+	defer mu.Unlock()
+
+	checks = []Config{}
+}
+
 func newCheck(status string, failures map[string]string) Check {
 	return Check{
 		Status:    status,
@@ -131,6 +157,7 @@ func newCheck(status string, failures map[string]string) Check {
 func newSystemMetrics() System {
 	s := runtime.MemStats{}
 	runtime.ReadMemStats(&s)
+
 	return System{
 		Version:          runtime.Version(),
 		GoroutinesCount:  runtime.NumGoroutine(),
