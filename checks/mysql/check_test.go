3f05b303539ae636f0e69b2cd74ec3f57d1339b4
Sergiu Ghitea <sg@hellofresh.com>
2018-07-12 00:59:38 +0200

    Update mysql check tests

--- checks/mysql/check_test.go
+++ checks/mysql/check_test.go
@@ -3,6 +3,7 @@ package mysql
 import (
 	"os"
 	"testing"
+	"time"
 )
 
 const mysqlDSNEnv = "HEALTH_GO_MS_DSN"
@@ -13,7 +14,17 @@ func TestNew(t *testing.T) {
 	}
 
 	check := New(Config{
-		DSN: os.Getenv(mysqlDSNEnv),
+		DSN:      os.Getenv(mysqlDSNEnv),
+		Table:    "test",
+		IDColumn: "id",
+		InsertColumnsFunc: func() map[string]interface{} {
+			return map[string]interface{}{
+				"id":           nil,
+				"secret":       time.Now().Format(time.RFC3339Nano),
+				"extra":        time.Now().Format(time.RFC3339Nano),
+				"redirect_uri": "http://localhost",
+			}
+		},
 	})
 
 	if err := check(); err != nil {
