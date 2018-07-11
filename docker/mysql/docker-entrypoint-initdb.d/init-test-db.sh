7b93c8414f9ef89e2d733fa555b76b9ea80fcbcd
Sergiu Ghitea <sg@hellofresh.com>
2018-07-12 01:00:32 +0200

    Update init db script for mysql
    Changed id to be an auto increment value so we can fetch the last inserted id. Also dropping the table if already exists for tests before recreating it.

--- docker/mysql/docker-entrypoint-initdb.d/init-test-db.sh
+++ docker/mysql/docker-entrypoint-initdb.d/init-test-db.sh
@@ -4,8 +4,10 @@ set -e
 mysql -v -uroot -p${MYSQL_ROOT_PASSWORD} <<-EOSQL
 USE ${MYSQL_DATABASE};
 
+DROP TABLE IF EXISTS test;
+
 CREATE TABLE IF NOT EXISTS test (
-  id           VARCHAR(256) NOT NULL PRIMARY KEY,
+  id           INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   secret       VARCHAR(256) NOT NULL,
   extra        VARCHAR(256) NOT NULL,
   redirect_uri VARCHAR(256) NOT NULL
