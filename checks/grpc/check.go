c60a67dd070bf44783d8315950a288e60bb4cac9
Vladimir Garvardt <vgarvardt@gmail.com>
2020-09-02 12:31:34 +0200

    Added gRPC check implementation

---
+++ checks/grpc/check.go
@@ -0,0 +1,59 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+const defaultCheckTimeout = 5 * time.Second
+
+// Config is the gRPC checker configuration settings container.
+type Config struct {
+	// Target is the address of the gRPC server
+	Target string
+	// Service is the name of the gRPC service
+	Service string
+	// DialOptions configure how we set up the connection
+	DialOptions []grpc.DialOption
+	// CheckTimeout is the duration that health check will try to get gRPC service health status.
+	// If not set - 5 seconds
+	CheckTimeout time.Duration
+}
+
+// New creates new gRPC health check
+func New(config Config) func() error {
+	if config.CheckTimeout == 0 {
+		config.CheckTimeout = defaultCheckTimeout
+	}
+
+	return func() error {
+		// Set up a connection to the gRPC server
+		conn, err := grpc.Dial(config.Target, config.DialOptions...)
+		if err != nil {
+			return fmt.Errorf("gRPC health check failed on connect: %w", err)
+		}
+		defer conn.Close()
+
+		healthClient := grpc_health_v1.NewHealthClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), config.CheckTimeout)
+		defer cancel()
+
+		res, err := healthClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{
+			Service: config.Service,
+		})
+		if err != nil {
+			return fmt.Errorf("gRPC health check failed on check call: %w", err)
+		}
+
+		if res.GetStatus() != grpc_health_v1.HealthCheckResponse_SERVING {
+			return fmt.Errorf("gRPC service reported as non-serving: %q", res.GetStatus().String())
+		}
+
+		return nil
+	}
+}
