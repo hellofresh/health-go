66c87794d0c742e3ea3fdf9f74ec2ad8ada50827
hosseio <y2josei@gmail.com>
2023-06-27 10:53:09 +0200

    feat: add check for nats connection

---
+++ checks/nats/check.go
@@ -0,0 +1,33 @@
+package nats
+
+import (
+	"context"
+	"fmt"
+	"github.com/nats-io/nats.go"
+)
+
+// Config is the NATS checker configuration settings container.
+type Config struct {
+	// DSN is the NATS instance connection DSN. Required.
+	DSN string
+}
+
+// New creates new NATS health check that verifies the status of the connection.
+func New(config Config) func(ctx context.Context) error {
+	return func(ctx context.Context) (checkErr error) {
+		nc, err := nats.Connect(config.DSN)
+		if err != nil {
+			checkErr = fmt.Errorf("nats health check failed on client creation: %w", err)
+			return
+		}
+		defer nc.Close()
+
+		status := nc.Status()
+		if status != nats.CONNECTED {
+			checkErr = fmt.Errorf("nats health check failed as connection status is %s", status)
+			return
+		}
+
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/jackc/pgx/v4 v4.18.1
 	github.com/jackc/pgx/v5 v5.4.1
 	github.com/lib/pq v1.10.9
+	github.com/nats-io/nats.go v1.27.1
 	github.com/rabbitmq/amqp091-go v1.8.1
 	github.com/stretchr/testify v1.8.4
 	github.com/vitorsalgado/mocha/v2 v2.0.2
@@ -58,6 +59,9 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.19 // indirect
+	github.com/nats-io/nkeys v0.4.4 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
