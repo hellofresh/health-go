6734a7a3f892e48381a5548f118274d5d6df2001
miya-masa <miyauchi0623@gmail.com>
2023-07-25 04:44:36 +0000

    feat(*): support TLS in redis checker.

--- checks/redis/check.go
+++ checks/redis/check.go
@@ -20,9 +20,11 @@ type Config struct {
 func New(config Config) func(ctx context.Context) error {
 	// support all DSN formats (for backward compatibility) - with and w/out schema and path part:
 	// - redis://localhost:1234/
+	// - rediss://localhost:1234/
 	// - localhost:1234
+
 	redisDSN := config.DSN
-	if !strings.HasPrefix(redisDSN, "redis://") {
+	if !strings.HasPrefix(redisDSN, "redis://") && !strings.HasPrefix(redisDSN, "rediss://") {
 		redisDSN = fmt.Sprintf("redis://%s", redisDSN)
 	}
 	redisOptions, _ := redis.ParseURL(redisDSN)
